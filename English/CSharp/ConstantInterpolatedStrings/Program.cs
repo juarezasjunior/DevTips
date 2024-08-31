52dbb6352ed0b43d1f8ea0568273b7107b9a459b
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-08-31 11:43:06 -0300

    ConstantInterpolatedStrings

---
+++ English/CSharp/ConstantInterpolatedStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    const string Name = "C#";
+    const string Version = "10";
+    
+    // Using interpolation in a constant
+    const string Greeting = $"Welcome to {Name} version {Version}!";
+
+    public static void Main()
+    {
+        Console.WriteLine(Greeting);
+    }
+}
---
+++ Portugues/CSharp/ConstantInterpolatedStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    const string Nome = "C#";
+    const string Versao = "10";
+    
+    // Usando interpolação em uma constante
+    const string Saudacao = $"Bem-vindo ao {Nome} versão {Versao}!";
+
+    public static void Main()
+    {
+        Console.WriteLine(Saudacao);
+    }
+}
