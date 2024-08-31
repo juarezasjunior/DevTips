027709998f6368dfd7fdc8fd65230dde99879b57
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-08-31 12:11:27 -0300

    ExpressionBodiedMembers

---
+++ English/CSharp/ExpressionBodiedMembers/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    // Using Expression-bodied Member for the ToString method
+    public override string ToString() => $"Product: {Name}, Price: {Price:C}";
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var product = new Product { Name = "Pen", Price = 2.99m };
+        Console.WriteLine(product);
+    }
+}
---
+++ Portugues/CSharp/ExpressionBodiedMembers/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Produto
+{
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+
+    // Usando Expression-bodied Member para o método ToString
+    public override string ToString() => $"Produto: {Nome}, Preço: {Preco:C}";
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var produto = new Produto { Nome = "Caneta", Preco = 2.99m };
+        Console.WriteLine(produto);
+    }
+}
