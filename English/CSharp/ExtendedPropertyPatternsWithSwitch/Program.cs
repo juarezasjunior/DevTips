6c3e21a2e79b7082fbf58d5b9876098045646f53
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-08-31 12:34:25 -0300

    ExtendedPropertyPatternsWithSwitch

---
+++ English/CSharp/ExtendedPropertyPatternsWithSwitch/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Product product = new Product { Name = "Pen", Price = 5.99m };
+
+        string result = product switch
+        {
+            { Price: > 10.00m } => "Expensive product",
+            { Price: <= 10.00m } => "Affordable product",
+            _ => "Price unknown"
+        };
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Portugues/CSharp/ExtendedPropertyPatternsWithSwitch/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Produto
+{
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Produto produto = new Produto { Nome = "Caneta", Preco = 5.99m };
+
+        string resultado = produto switch
+        {
+            { Preco: > 10.00m } => "Produto caro",
+            { Preco: <= 10.00m } => "Produto acessível",
+            _ => "Preço desconhecido"
+        };
+
+        Console.WriteLine(resultado);
+    }
+}
