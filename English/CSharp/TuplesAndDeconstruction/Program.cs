adc3187db1ba523e1110f94b073fe240dd29623c
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-08-31 12:44:05 -0300

    Tuples

---
+++ English/CSharp/TuplesAndDeconstruction/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var (name, price) = GetProduct();
+
+        Console.WriteLine($"Product: {name}, Price: {price}");
+    }
+
+    public static (string, decimal) GetProduct()
+    {
+        return ("Pen", 2.99m);
+    }
+}
---
+++ Portugues/CSharp/TuplesAndDeconstruction/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var (nome, preco) = ObterProduto();
+
+        Console.WriteLine($"Produto: {nome}, Preço: {preco}");
+    }
+
+    public static (string, decimal) ObterProduto()
+    {
+        return ("Caneta", 2.99m);
+    }
+}
