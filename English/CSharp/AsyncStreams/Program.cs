7c823b66f82825af3b3a1c492948c81a9c1aad13
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-08-31 11:59:27 -0300

    AsyncStreams

---
+++ English/CSharp/AsyncStreams/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        await foreach (var number in GenerateNumbersAsync())
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    public static async IAsyncEnumerable<int> GenerateNumbersAsync()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            await Task.Delay(1000); // Simulates an asynchronous operation
+            yield return i;
+        }
+    }
+}
---
+++ Portugues/CSharp/AsyncStreams/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        await foreach (var numero in GerarNumerosAsync())
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    public static async IAsyncEnumerable<int> GerarNumerosAsync()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            await Task.Delay(1000); // Simula uma operação assíncrona
+            yield return i;
+        }
+    }
+}
