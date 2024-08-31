dfe51b23cfd55696e3bd7e62128c7d212a718638
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-08-31 12:21:39 -0300

    LocalFunctions

---
+++ English/CSharp/LocalFunctions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number = 5;
+        Console.WriteLine($"Factorial of {number} is {CalculateFactorial(number)}");
+
+        // Local function to calculate factorial
+        int CalculateFactorial(int n)
+        {
+            if (n <= 1) return 1;
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
---
+++ Portugues/CSharp/LocalFunctions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int numero = 5;
+        Console.WriteLine($"Fatorial de {numero} é {CalcularFatorial(numero)}");
+
+        // Função local para calcular o fatorial
+        int CalcularFatorial(int n)
+        {
+            if (n <= 1) return 1;
+            return n * CalcularFatorial(n - 1);
+        }
+    }
+}
