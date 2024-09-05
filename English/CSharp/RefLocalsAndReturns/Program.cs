d7ef0cff69b968d799443afc3e20dacc0ede0b74
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-04 22:07:28 -0300

    RefLocalsAndReturns

---
+++ English/CSharp/RefLocalsAndReturns/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 10, 20, 30 };
+        
+        // Get a reference to the second array element and modify its value
+        ref int reference = ref GetReference(numbers, 1);
+        reference = 50;
+        
+        Console.WriteLine(string.Join(", ", numbers)); // Output: 10, 50, 30
+    }
+
+    public static ref int GetReference(int[] array, int index)
+    {
+        return ref array[index]; // Returns the reference to the array element
+    }
+}
---
+++ Portugues/CSharp/RefLocalsAndReturns/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numeros = { 10, 20, 30 };
+        
+        // Obtém uma referência ao segundo elemento do array e modifica seu valor
+        ref int referencia = ref ObterReferencia(numeros, 1);
+        referencia = 50;
+        
+        Console.WriteLine(string.Join(", ", numeros)); // Saída: 10, 50, 30
+    }
+
+    public static ref int ObterReferencia(int[] array, int indice)
+    {
+        return ref array[indice]; // Retorna a referência ao elemento do array
+    }
+}
