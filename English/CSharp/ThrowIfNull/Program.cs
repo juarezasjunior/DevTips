a1556daca5309db80ccf2e8599e57fc848cf3b21
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-09 22:26:31 -0300

    ThrowIfNull

---
+++ English/CSharp/ThrowIfNull/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string name = null;
+
+        // Checks and throws an ArgumentNullException if the argument is null
+        ArgumentNullException.ThrowIfNull(name, nameof(name));
+
+        Console.WriteLine($"Name: {name}");
+    }
+}
---
+++ Portugues/CSharp/ThrowIfNull/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string nome = null;
+
+        // Verifica e lança uma ArgumentNullException se o argumento for null
+        ArgumentNullException.ThrowIfNull(nome, nameof(nome));
+
+        Console.WriteLine($"Nome: {nome}");
+    }
+}
