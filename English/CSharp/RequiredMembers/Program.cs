207f66f22ef10b1ea76d1c8c1a5ea240f48cd75c
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 10:13:42 -0300

    RequiredMembers

---
+++ English/CSharp/RequiredMembers/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Product
+{
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Product product = new Product
+        {
+            Name = "Pen",
+            // An exception will be thrown when compiling
+            //Price = 2.99m
+        };
+
+        Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+    }
+}
---
+++ Portugues/CSharp/RequiredMembers/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Produto
+{
+    public required string Nome { get; set; }
+    public required decimal Preco { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Produto produto = new Produto
+        {
+            Nome = "Caneta",
+            // Vai gerar um erro ao compilar
+            //Preco = 2.99m
+        };
+
+        Console.WriteLine($"Produto: {produto.Nome}, Preço: {produto.Preco}");
+    }
+}
