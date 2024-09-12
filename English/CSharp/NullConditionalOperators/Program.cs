64e6d68ade9b588cd783a0e88b0d00664e483c83
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 11:00:22 -0300

    NullConditionalOperators

---
+++ English/CSharp/NullConditionalOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Product
+{
+    public string Name { get; set; }
+    public decimal? Price { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Product product = null;
+        // Using null-conditional operator to safely access members
+        Console.WriteLine(product?.Name ?? "Product not specified");  // Output: Product not specified
+
+        product = new Product { Name = "Pen", Price = 2.99m };
+        Console.WriteLine(product?.Name);  // Output: Pen
+    }
+}
---
+++ Portugues/CSharp/NullConditionalOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Produto
+{
+    public string Nome { get; set; }
+    public decimal? Preco { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Produto produto = null;
+        // Usando operador null-conditional para acessar membros de forma segura
+        Console.WriteLine(produto?.Nome ?? "Produto não informado");  // Saída: Produto não informado
+
+        produto = new Produto { Nome = "Caneta", Preco = 2.99m };
+        Console.WriteLine(produto?.Nome);  // Saída: Caneta
+    }
+}
