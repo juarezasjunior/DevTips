dadd0477ebb908dfa2f92ba1f2898045714dbc2c
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 15:18:42 -0300

    AbstractFactory

---
+++ English/CSharp Design Patterns/AbstractFactory/Program.cs
@@ -0,0 +1,102 @@
+﻿// Abstract product 1
+public abstract class Car
+{
+    public abstract void ShowDetails();
+}
+
+// Concrete product 1A
+public class Sedan : Car
+{
+    public override void ShowDetails()
+    {
+        Console.WriteLine("Sedan Car");
+    }
+}
+
+// Concrete product 1B
+public class SUV : Car
+{
+    public override void ShowDetails()
+    {
+        Console.WriteLine("SUV Car");
+    }
+}
+
+// Abstract product 2
+public abstract class Engine
+{
+    public abstract void ShowType();
+}
+
+// Concrete product 2A
+public class CombustionEngine : Engine
+{
+    public override void ShowType()
+    {
+        Console.WriteLine("Combustion Engine");
+    }
+}
+
+// Concrete product 2B
+public class ElectricEngine : Engine
+{
+    public override void ShowType()
+    {
+        Console.WriteLine("Electric Engine");
+    }
+}
+
+// Abstract factory
+public abstract class VehicleFactory
+{
+    public abstract Car CreateCar();
+    public abstract Engine CreateEngine();
+}
+
+// Concrete factory 1
+public class SedanCombustionFactory : VehicleFactory
+{
+    public override Car CreateCar()
+    {
+        return new Sedan();
+    }
+
+    public override Engine CreateEngine()
+    {
+        return new CombustionEngine();
+    }
+}
+
+// Concrete factory 2
+public class SUVElectricFactory : VehicleFactory
+{
+    public override Car CreateCar()
+    {
+        return new SUV();
+    }
+
+    public override Engine CreateEngine()
+    {
+        return new ElectricEngine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        VehicleFactory factory = new SedanCombustionFactory();
+        Car car = factory.CreateCar();
+        Engine engine = factory.CreateEngine();
+
+        car.ShowDetails();   // Output: Sedan Car
+        engine.ShowType();   // Output: Combustion Engine
+
+        factory = new SUVElectricFactory();
+        car = factory.CreateCar();
+        engine = factory.CreateEngine();
+
+        car.ShowDetails();   // Output: SUV Car
+        engine.ShowType();   // Output: Electric Engine
+    }
+}
---
+++ Portugues/CSharp Design Patterns/AbstractFactory/Program.cs
@@ -0,0 +1,102 @@
+﻿// Produto abstrato 1
+public abstract class Carro
+{
+    public abstract void ExibirDetalhes();
+}
+
+// Produto concreto 1A
+public class Sedan : Carro
+{
+    public override void ExibirDetalhes()
+    {
+        Console.WriteLine("Carro Sedan");
+    }
+}
+
+// Produto concreto 1B
+public class SUV : Carro
+{
+    public override void ExibirDetalhes()
+    {
+        Console.WriteLine("Carro SUV");
+    }
+}
+
+// Produto abstrato 2
+public abstract class Motor
+{
+    public abstract void ExibirTipo();
+}
+
+// Produto concreto 2A
+public class MotorCombustao : Motor
+{
+    public override void ExibirTipo()
+    {
+        Console.WriteLine("Motor a combustão");
+    }
+}
+
+// Produto concreto 2B
+public class MotorEletrico : Motor
+{
+    public override void ExibirTipo()
+    {
+        Console.WriteLine("Motor elétrico");
+    }
+}
+
+// Fábrica abstrata
+public abstract class VeiculoFactory
+{
+    public abstract Carro CriarCarro();
+    public abstract Motor CriarMotor();
+}
+
+// Fábrica concreta 1
+public class SedanCombustaoFactory : VeiculoFactory
+{
+    public override Carro CriarCarro()
+    {
+        return new Sedan();
+    }
+
+    public override Motor CriarMotor()
+    {
+        return new MotorCombustao();
+    }
+}
+
+// Fábrica concreta 2
+public class SUVEletricoFactory : VeiculoFactory
+{
+    public override Carro CriarCarro()
+    {
+        return new SUV();
+    }
+
+    public override Motor CriarMotor()
+    {
+        return new MotorEletrico();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        VeiculoFactory factory = new SedanCombustaoFactory();
+        Carro carro = factory.CriarCarro();
+        Motor motor = factory.CriarMotor();
+
+        carro.ExibirDetalhes(); // Saída: Carro Sedan
+        motor.ExibirTipo();     // Saída: Motor a combustão
+
+        factory = new SUVEletricoFactory();
+        carro = factory.CriarCarro();
+        motor = factory.CriarMotor();
+
+        carro.ExibirDetalhes(); // Saída: Carro SUV
+        motor.ExibirTipo();     // Saída: Motor elétrico
+    }
+}
