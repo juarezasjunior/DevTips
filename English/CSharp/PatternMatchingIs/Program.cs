56432a5bccb95af04cabb91dc976f6e5566d5f9b
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 13:38:35 -0300

    PatternMatchingIs

---
+++ English/CSharp/PatternMatchingIs/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        object value = 123;
+
+        if (value is int number)
+        {
+            Console.WriteLine($"The value is a number: {number}");
+        }
+        else
+        {
+            Console.WriteLine("The value is not a number.");
+        }
+    }
+}
---
+++ Portugues/CSharp/PatternMatchingIs/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        object valor = 123;
+
+        if (valor is int numero)
+        {
+            Console.WriteLine($"O valor é um número: {numero}");
+        }
+        else
+        {
+            Console.WriteLine("O valor não é um número.");
+        }
+    }
+}
