3ca7ba196eb55b7642926a8a178f7b6512204406
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 13:25:45 -0300

    AnonymousTypes

---
+++ English/CSharp/AnonymousTypes/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var product = new { Name = "Pen", Price = 2.99m };
+
+        Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+    }
+}
---
+++ Portugues/CSharp/AnonymousTypes/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var produto = new { Nome = "Caneta", Preco = 2.99m };
+
+        Console.WriteLine($"Produto: {produto.Nome}, Preço: {produto.Preco}");
+    }
+}
