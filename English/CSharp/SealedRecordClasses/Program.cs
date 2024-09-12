b129b821584e2d7682247e852993deaeb236d78c
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 10:42:53 -0300

    SelaedRecordClasses

---
+++ English/CSharp/SealedRecordClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿public sealed record Product(string Name, decimal Price);
+
+// You cannot inherit from Product
+//public record Pen(string color) : Product;
+
+public class Program
+{
+    public static void Main()
+    {
+        Product product = new("Pen", 2.99m);
+        Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+    }
+}
---
+++ Portugues/CSharp/SealedRecordClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿public sealed record Produto(string Nome, decimal Preco);
+
+// Você não pode fazer herança
+//public record Caneta(string cor) : Produto;
+
+public class Program
+{
+    public static void Main()
+    {
+        Produto produto = new("Caneta", 2.99m);
+        Console.WriteLine($"Produto: {produto.Nome}, Preço: {produto.Preco}");
+    }
+}
