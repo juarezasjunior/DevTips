faac7361600c3ee9498e2b415bebac9b0ae7bbbd
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 14:05:25 -0300

    Singleton

---
+++ English/CSharp Design Patterns/Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Singleton
+{
+    private static Singleton _instance;
+
+    // Private constructor to prevent external instantiation
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+    public void ShowMessage()
+    {
+        Console.WriteLine("Singleton instance active!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Singleton s1 = Singleton.Instance;
+        Singleton s2 = Singleton.Instance;
+
+        s1.ShowMessage();
+
+        // Check if both instances are the same
+        Console.WriteLine(s1 == s2);  // True
+    }
+}
---
+++ Portugues/CSharp Design Patterns/Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Singleton
+{
+    private static Singleton _instance;
+
+    // Construtor privado para impedir a criação de instâncias externas
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+    public void ShowMessage()
+    {
+        Console.WriteLine("Singleton instance ativa!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Singleton s1 = Singleton.Instance;
+        Singleton s2 = Singleton.Instance;
+
+        s1.ShowMessage();
+
+        // Verificar se ambas as instâncias são iguais
+        Console.WriteLine(s1 == s2);  // True
+    }
+}
