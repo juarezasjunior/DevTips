f963efea4246f9e380e42942728bdc8642558184
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 11:31:03 -0300

    EnhancedUsings

---
+++ English/CSharp/EnhancedUsings/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        using var file = new System.IO.StreamWriter("file.txt");
+        file.WriteLine("Writing to the file without using blocks.");
+
+        // The StreamWriter will automatically be closed at the end of the method
+    }
+}
---
+++ Portugues/CSharp/EnhancedUsings/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        using var arquivo = new System.IO.StreamWriter("arquivo.txt");
+        arquivo.WriteLine("Escrevendo no arquivo sem usar blocos de using.");
+
+        // O StreamWriter será automaticamente fechado ao final do método
+    }
+}
