2e23bec65b93029030704b9e464e47a499c7c894
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-12 11:17:34 -0300

    TopLevelStatements

---
+++ English/CSharp/TopLevelStatements/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, world!");
+int result = Add(5, 10);
+Console.WriteLine($"The result of the addition is: {result}");
+
+int Add(int a, int b)
+{
+    return a + b;
+}
---
+++ Portugues/CSharp/TopLevelStatements/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Olá, mundo!");
+int resultado = Somar(5, 10);
+Console.WriteLine($"O resultado da soma é: {resultado}");
+
+int Somar(int a, int b)
+{
+    return a + b;
+}
