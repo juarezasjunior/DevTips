1ba2e62a0a02e35cea16d6d1115c445d8ee49826
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-26 11:13:45 -0300

    Facade

---
+++ English/CSharp Design Patterns/Facade/Program.cs
@@ -0,0 +1,83 @@
+﻿// Complex subsystem: TV
+public class TV
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("TV turned on.");
+    }
+
+    public void SetChannel(int channel)
+    {
+        Console.WriteLine($"Channel set to {channel}.");
+    }
+}
+
+// Complex subsystem: DVD player
+public class DVDPlayer
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("DVD player turned on.");
+    }
+
+    public void Play()
+    {
+        Console.WriteLine("Playing DVD.");
+    }
+}
+
+// Complex subsystem: Sound system
+public class SoundSystem
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Sound system turned on.");
+    }
+
+    public void SetVolume(int volume)
+    {
+        Console.WriteLine($"Volume set to {volume}.");
+    }
+}
+
+// Facade: Home Theater
+public class HomeTheaterFacade
+{
+    private TV _tv;
+    private DVDPlayer _dvdPlayer;
+    private SoundSystem _soundSystem;
+
+    public HomeTheaterFacade(TV tv, DVDPlayer dvdPlayer, SoundSystem soundSystem)
+    {
+        _tv = tv;
+        _dvdPlayer = dvdPlayer;
+        _soundSystem = soundSystem;
+    }
+
+    public void WatchMovie()
+    {
+        Console.WriteLine("Starting Home Theater...");
+        _tv.TurnOn();
+        _tv.SetChannel(3);
+        _dvdPlayer.TurnOn();
+        _dvdPlayer.Play();
+        _soundSystem.TurnOn();
+        _soundSystem.SetVolume(20);
+        Console.WriteLine("All set, enjoy your movie!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating the subsystems
+        TV tv = new TV();
+        DVDPlayer dvdPlayer = new DVDPlayer();
+        SoundSystem soundSystem = new SoundSystem();
+
+        // Using the facade
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(tv, dvdPlayer, soundSystem);
+        homeTheater.WatchMovie();
+    }
+}
---
+++ Portugues/CSharp Design Patterns/Facade/Program.cs
@@ -0,0 +1,83 @@
+﻿// Subsistema complexo: TV
+public class TV
+{
+    public void Ligar()
+    {
+        Console.WriteLine("TV ligada.");
+    }
+
+    public void DefinirCanal(int canal)
+    {
+        Console.WriteLine($"Canal definido para {canal}.");
+    }
+}
+
+// Subsistema complexo: DVD player
+public class DVDPlayer
+{
+    public void Ligar()
+    {
+        Console.WriteLine("DVD player ligado.");
+    }
+
+    public void Reproduzir()
+    {
+        Console.WriteLine("Reproduzindo DVD.");
+    }
+}
+
+// Subsistema complexo: Sistema de Som
+public class SistemaDeSom
+{
+    public void Ligar()
+    {
+        Console.WriteLine("Sistema de som ligado.");
+    }
+
+    public void DefinirVolume(int volume)
+    {
+        Console.WriteLine($"Volume definido para {volume}.");
+    }
+}
+
+// Facade: Home Theater
+public class HomeTheaterFacade
+{
+    private TV _tv;
+    private DVDPlayer _dvdPlayer;
+    private SistemaDeSom _som;
+
+    public HomeTheaterFacade(TV tv, DVDPlayer dvdPlayer, SistemaDeSom som)
+    {
+        _tv = tv;
+        _dvdPlayer = dvdPlayer;
+        _som = som;
+    }
+
+    public void AssistirFilme()
+    {
+        Console.WriteLine("Iniciando o Home Theater...");
+        _tv.Ligar();
+        _tv.DefinirCanal(3);
+        _dvdPlayer.Ligar();
+        _dvdPlayer.Reproduzir();
+        _som.Ligar();
+        _som.DefinirVolume(20);
+        Console.WriteLine("Tudo pronto, aproveite o filme!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando os subsistemas
+        TV tv = new TV();
+        DVDPlayer dvdPlayer = new DVDPlayer();
+        SistemaDeSom som = new SistemaDeSom();
+
+        // Usando a facade
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(tv, dvdPlayer, som);
+        homeTheater.AssistirFilme();
+    }
+}
