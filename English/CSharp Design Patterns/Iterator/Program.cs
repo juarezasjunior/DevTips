79ef19a8d745ba6522244a353180773932b28873
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-26 12:32:23 -0300

    Iterator

---
+++ English/CSharp Design Patterns/Iterator/Program.cs
@@ -0,0 +1,80 @@
+﻿// Iterator Interface
+public interface IIterator<T>
+{
+    bool HasNext();
+    T Next();
+}
+
+// Collection Interface
+public interface ICollection<T>
+{
+    IIterator<T> CreateIterator();
+}
+
+// Implementation of Item Collection
+public class ItemCollection : ICollection<string>
+{
+    private List<string> _items = new List<string>();
+
+    public void AddItem(string item)
+    {
+        _items.Add(item);
+    }
+
+    public IIterator<string> CreateIterator()
+    {
+        return new ItemIterator(this);
+    }
+
+    public List<string> GetItems()
+    {
+        return _items;
+    }
+}
+
+// Implementation of Iterator for the Item Collection
+public class ItemIterator : IIterator<string>
+{
+    private ItemCollection _collection;
+    private int _position = 0;
+
+    public ItemIterator(ItemCollection collection)
+    {
+        _collection = collection;
+    }
+
+    public bool HasNext()
+    {
+        return _position < _collection.GetItems().Count;
+    }
+
+    public string Next()
+    {
+        if (HasNext())
+        {
+            return _collection.GetItems()[_position++];
+        }
+        return null;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an item collection
+        ItemCollection collection = new ItemCollection();
+        collection.AddItem("Item 1");
+        collection.AddItem("Item 2");
+        collection.AddItem("Item 3");
+
+        // Create the iterator
+        IIterator<string> iterator = collection.CreateIterator();
+
+        // Traverse the collection using the iterator
+        while (iterator.HasNext())
+        {
+            Console.WriteLine(iterator.Next());
+        }
+    }
+}
---
+++ Portugues/CSharp Design Patterns/Iterator/Program.cs
@@ -0,0 +1,80 @@
+﻿// Interface do Iterador
+public interface IIterador<T>
+{
+    bool HasNext();
+    T Next();
+}
+
+// Interface da Coleção
+public interface IColecao<T>
+{
+    IIterador<T> CriarIterador();
+}
+
+// Implementação da Coleção de Itens
+public class ColecaoDeItens : IColecao<string>
+{
+    private List<string> _itens = new List<string>();
+
+    public void AdicionarItem(string item)
+    {
+        _itens.Add(item);
+    }
+
+    public IIterador<string> CriarIterador()
+    {
+        return new IteradorDeItens(this);
+    }
+
+    public List<string> GetItens()
+    {
+        return _itens;
+    }
+}
+
+// Implementação do Iterador para a Coleção de Itens
+public class IteradorDeItens : IIterador<string>
+{
+    private ColecaoDeItens _colecao;
+    private int _posicao = 0;
+
+    public IteradorDeItens(ColecaoDeItens colecao)
+    {
+        _colecao = colecao;
+    }
+
+    public bool HasNext()
+    {
+        return _posicao < _colecao.GetItens().Count;
+    }
+
+    public string Next()
+    {
+        if (HasNext())
+        {
+            return _colecao.GetItens()[_posicao++];
+        }
+        return null;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criar uma coleção de itens
+        ColecaoDeItens colecao = new ColecaoDeItens();
+        colecao.AdicionarItem("Item 1");
+        colecao.AdicionarItem("Item 2");
+        colecao.AdicionarItem("Item 3");
+
+        // Criar o iterador
+        IIterador<string> iterador = colecao.CriarIterador();
+
+        // Percorrer a coleção usando o iterador
+        while (iterador.HasNext())
+        {
+            Console.WriteLine(iterador.Next());
+        }
+    }
+}
