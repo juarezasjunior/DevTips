454abf9aa32e113d6589cfb069ff3c9f12ff6eca
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-26 09:37:16 -0300

    ObjectPool

---
+++ English/CSharp Design Patterns/ObjectPool/Program.cs
@@ -0,0 +1,79 @@
+﻿// Example class to be managed by the Object Pool
+public class Connection
+{
+    public string Id { get; set; }
+
+    public Connection(string id)
+    {
+        Id = id;
+        Console.WriteLine($"Connection {Id} created.");
+    }
+
+    public void Connect()
+    {
+        Console.WriteLine($"Connection {Id} connected.");
+    }
+
+    public void Disconnect()
+    {
+        Console.WriteLine($"Connection {Id} disconnected.");
+    }
+}
+
+// Object Pool class to manage connections
+public class ConnectionPool
+{
+    private List<Connection> _available = new List<Connection>();
+    private List<Connection> _inUse = new List<Connection>();
+
+    public Connection GetConnection()
+    {
+        Connection connection;
+
+        if (_available.Count > 0)
+        {
+            connection = _available[0];
+            _available.RemoveAt(0);
+            Console.WriteLine($"Connection {connection.Id} reused.");
+        }
+        else
+        {
+            connection = new Connection(Guid.NewGuid().ToString());
+        }
+
+        _inUse.Add(connection);
+        return connection;
+    }
+
+    public void ReleaseConnection(Connection connection)
+    {
+        _inUse.Remove(connection);
+        _available.Add(connection);
+        Console.WriteLine($"Connection {connection.Id} released and returned to pool.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConnectionPool pool = new ConnectionPool();
+
+        // Get and use connections
+        Connection connection1 = pool.GetConnection();
+        connection1.Connect();
+
+        Connection connection2 = pool.GetConnection();
+        connection2.Connect();
+
+        // Release connections back to the pool
+        connection1.Disconnect();
+        pool.ReleaseConnection(connection1);
+
+        Connection connection3 = pool.GetConnection();  // Reusing released connection
+        connection3.Connect();
+
+        connection2.Disconnect();
+        pool.ReleaseConnection(connection2);
+    }
+}
---
+++ Portugues/CSharp Design Patterns/ObjectPool/Program.cs
@@ -0,0 +1,79 @@
+﻿// Classe de exemplo que será gerenciada pelo Object Pool
+public class Conexao
+{
+    public string Id { get; set; }
+
+    public Conexao(string id)
+    {
+        Id = id;
+        Console.WriteLine($"Conexão {Id} criada.");
+    }
+
+    public void Conectar()
+    {
+        Console.WriteLine($"Conexão {Id} conectada.");
+    }
+
+    public void Desconectar()
+    {
+        Console.WriteLine($"Conexão {Id} desconectada.");
+    }
+}
+
+// Classe Object Pool para gerenciar as conexões
+public class ConexaoPool
+{
+    private List<Conexao> _disponiveis = new List<Conexao>();
+    private List<Conexao> _emUso = new List<Conexao>();
+
+    public Conexao ObterConexao()
+    {
+        Conexao conexao;
+
+        if (_disponiveis.Count > 0)
+        {
+            conexao = _disponiveis[0];
+            _disponiveis.RemoveAt(0);
+            Console.WriteLine($"Conexão {conexao.Id} reutilizada.");
+        }
+        else
+        {
+            conexao = new Conexao(Guid.NewGuid().ToString());
+        }
+
+        _emUso.Add(conexao);
+        return conexao;
+    }
+
+    public void LiberarConexao(Conexao conexao)
+    {
+        _emUso.Remove(conexao);
+        _disponiveis.Add(conexao);
+        Console.WriteLine($"Conexão {conexao.Id} liberada e retornada ao pool.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConexaoPool pool = new ConexaoPool();
+
+        // Obter e usar conexões
+        Conexao conexao1 = pool.ObterConexao();
+        conexao1.Conectar();
+
+        Conexao conexao2 = pool.ObterConexao();
+        conexao2.Conectar();
+
+        // Liberar conexões de volta ao pool
+        conexao1.Desconectar();
+        pool.LiberarConexao(conexao1);
+
+        Conexao conexao3 = pool.ObterConexao();  // Reutilizando conexão liberada
+        conexao3.Conectar();
+
+        conexao2.Desconectar();
+        pool.LiberarConexao(conexao2);
+    }
+}
