cfa9a84654f3a5a8fba106e0039787a9458ac026
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-09-26 10:47:33 -0300

    Composite

---
+++ English/CSharp Design Patterns/Composite/Program.cs
@@ -0,0 +1,64 @@
+﻿// Base component
+public interface IComponent
+{
+    void Display();
+}
+
+// Leaf object (a file, for example)
+public class File : IComponent
+{
+    private string _name;
+
+    public File(string name)
+    {
+        _name = name;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"File: {_name}");
+    }
+}
+
+// Composite object (a folder, for example)
+public class Folder : IComponent
+{
+    private string _name;
+    private List<IComponent> _components = new List<IComponent>();
+
+    public Folder(string name)
+    {
+        _name = name;
+    }
+
+    public void Add(IComponent component)
+    {
+        _components.Add(component);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Folder: {_name}");
+        foreach (var component in _components)
+        {
+            component.Display();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create files and folders
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        
+        Folder folder1 = new Folder("Documents");
+        folder1.Add(file1);
+        folder1.Add(file2);
+
+        // Display folder structure
+        folder1.Display();
+    }
+}
---
+++ Portugues/CSharp Design Patterns/Composite/Program.cs
@@ -0,0 +1,64 @@
+﻿// Componente base
+public interface IComponente
+{
+    void Exibir();
+}
+
+// Objeto folha (um arquivo, por exemplo)
+public class Arquivo : IComponente
+{
+    private string _nome;
+
+    public Arquivo(string nome)
+    {
+        _nome = nome;
+    }
+
+    public void Exibir()
+    {
+        Console.WriteLine($"Arquivo: {_nome}");
+    }
+}
+
+// Objeto composto (uma pasta, por exemplo)
+public class Pasta : IComponente
+{
+    private string _nome;
+    private List<IComponente> _componentes = new List<IComponente>();
+
+    public Pasta(string nome)
+    {
+        _nome = nome;
+    }
+
+    public void Adicionar(IComponente componente)
+    {
+        _componentes.Add(componente);
+    }
+
+    public void Exibir()
+    {
+        Console.WriteLine($"Pasta: {_nome}");
+        foreach (var componente in _componentes)
+        {
+            componente.Exibir();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criar arquivos e pastas
+        Arquivo arquivo1 = new Arquivo("arquivo1.txt");
+        Arquivo arquivo2 = new Arquivo("arquivo2.txt");
+        
+        Pasta pasta1 = new Pasta("Documentos");
+        pasta1.Adicionar(arquivo1);
+        pasta1.Adicionar(arquivo2);
+
+        // Exibir estrutura da pasta
+        pasta1.Exibir();
+    }
+}
