7787d376d613bda1c932ca615eecc638364f2f55
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-17 09:14:21 -0300

    NLog

---
+++ English/CSharp Libraries/NLogLibrary/Program.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using System;
+
+namespace NLogExample
+{
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            // Configuring NLog
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Target: file
+            var logFile = new NLog.Targets.FileTarget("logFile") { FileName = "log.txt" };
+
+            // Logging rules
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logFile);
+
+            // Applying the configuration
+            LogManager.Configuration = config;
+
+            // Example logs
+            logger.Info("The application has started.");
+            logger.Warn("This is a warning.");
+            logger.Error("An error occurred.");
+
+            Console.WriteLine("Logs generated. Check the log.txt file.");
+        }
+    }
+}
---
+++ Portugues/CSharp Libraries/NLogLibrary/Program.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using System;
+
+namespace NLogExemplo
+{
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            // Configurando NLog
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Destino: arquivo
+            var logArquivo = new NLog.Targets.FileTarget("logArquivo") { FileName = "log.txt" };
+
+            // Regras de logging
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logArquivo);
+
+            // Aplicando a configuração
+            LogManager.Configuration = config;
+
+            // Exemplo de logs
+            logger.Info("A aplicação começou.");
+            logger.Warn("Este é um aviso.");
+            logger.Error("Um erro ocorreu.");
+
+            Console.WriteLine("Logs gerados. Verifique o arquivo log.txt.");
+        }
+    }
+}
