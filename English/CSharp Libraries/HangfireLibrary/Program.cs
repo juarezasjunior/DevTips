e9d5dd8602f8d7a203750c58f85ebbf4511c0025
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-17 10:22:52 -0300

    Hangfire

---
+++ English/CSharp Libraries/HangfireLibrary/Program.cs
@@ -0,0 +1,37 @@
+﻿using Hangfire;
+using Hangfire.MemoryStorage;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Configuring Hangfire with in-memory storage
+        GlobalConfiguration.Configuration.UseMemoryStorage();
+
+        using (var server = new BackgroundJobServer())
+        {
+            // Scheduling a job for immediate execution
+            BackgroundJob.Enqueue(() => BackgroundTask());
+
+            // Scheduling a recurring job (executed every minute)
+            RecurringJob.AddOrUpdate("recurring-task", () => RecurringTask(), Cron.Minutely);
+
+            Console.WriteLine("Jobs scheduled. Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+
+    // Task that will run in the background
+    public static void BackgroundTask()
+    {
+        Console.WriteLine("Background task executed.");
+    }
+
+    // Recurring task
+    public static void RecurringTask()
+    {
+        Console.WriteLine("Recurring task executed.");
+    }
+}
---
+++ Portugues/CSharp Libraries/HangfireLibrary/Program.cs
@@ -0,0 +1,37 @@
+﻿using Hangfire;
+using Hangfire.MemoryStorage;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Configurando Hangfire com armazenamento em memória
+        GlobalConfiguration.Configuration.UseMemoryStorage();
+
+        using (var server = new BackgroundJobServer())
+        {
+            // Agendando um job para execução imediata
+            BackgroundJob.Enqueue(() => TarefaEmSegundoPlano());
+
+            // Agendando um job recorrente (executado a cada minuto)
+            RecurringJob.AddOrUpdate("tarefa-recorrente", () => TarefaRecorrente(), Cron.Minutely);
+
+            Console.WriteLine("Jobs agendados. Pressione Enter para sair...");
+            Console.ReadLine();
+        }
+    }
+
+    // Tarefa que será executada em segundo plano
+    public static void TarefaEmSegundoPlano()
+    {
+        Console.WriteLine("Tarefa em segundo plano executada.");
+    }
+
+    // Tarefa recorrente
+    public static void TarefaRecorrente()
+    {
+        Console.WriteLine("Tarefa recorrente executada.");
+    }
+}
