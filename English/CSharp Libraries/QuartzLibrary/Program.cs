ca33a8e5298651262d0db74d567c6cb6f20974e5
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-17 10:34:30 -0300

    Quartz

---
+++ English/CSharp Libraries/QuartzLibrary/Program.cs
@@ -0,0 +1,43 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+public class ExampleTask : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine($"Task executed at: {DateTime.Now}");
+        return Task.CompletedTask;
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Creating a task scheduler
+        IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+        await scheduler.Start();
+
+        // Defining the job to be scheduled
+        IJobDetail job = JobBuilder.Create<ExampleTask>()
+            .WithIdentity("exampleTask", "group1")
+            .Build();
+
+        // Creating a cron trigger to execute the task every minute
+        ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("triggerExample", "group1")
+            .StartNow()
+            .WithCronSchedule("0 * * ? * *") // Executes every minute
+            .Build();
+
+        // Scheduling the job
+        await scheduler.ScheduleJob(job, trigger);
+
+        Console.WriteLine("Task scheduled. Press Enter to exit...");
+        Console.ReadLine();
+
+        await scheduler.Shutdown();
+    }
+}
---
+++ Portugues/CSharp Libraries/QuartzLibrary/Program.cs
@@ -0,0 +1,43 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+public class TarefaExemplo : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine($"Tarefa executada em: {DateTime.Now}");
+        return Task.CompletedTask;
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Criando um agendador de tarefas
+        IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+        await scheduler.Start();
+
+        // Definindo a tarefa que será agendada
+        IJobDetail job = JobBuilder.Create<TarefaExemplo>()
+            .WithIdentity("tarefaExemplo", "grupo1")
+            .Build();
+
+        // Criando um cron trigger para execução a cada minuto
+        ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("triggerExemplo", "grupo1")
+            .StartNow()
+            .WithCronSchedule("0 * * ? * *") // Executa todo minuto
+            .Build();
+
+        // Agendando a tarefa
+        await scheduler.ScheduleJob(job, trigger);
+
+        Console.WriteLine("Tarefa agendada. Pressione Enter para sair...");
+        Console.ReadLine();
+
+        await scheduler.Shutdown();
+    }
+}
