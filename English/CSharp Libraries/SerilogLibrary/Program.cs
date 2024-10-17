a083a0d2a58809dc7c72c803ccd4eb4ba69fe9b5
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-17 09:27:58 -0300

    Serilog

---
+++ English/CSharp Libraries/SerilogLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using System;
+
+namespace SerilogExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configuring Serilog to log to a file
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("serilog-log.txt")
+                .CreateLogger();
+
+            // Example logs
+            Log.Information("The application has started.");
+            Log.Warning("This is a warning.");
+            Log.Error("An error occurred in the process.");
+
+            Console.WriteLine("Logs generated. Check the serilog-log.txt file.");
+
+            // Closing the logger
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ Portugues/CSharp Libraries/SerilogLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using System;
+
+namespace SerilogExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configurando o Serilog para registrar logs em um arquivo
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("serilog-log.txt")
+                .CreateLogger();
+
+            // Exemplos de logs
+            Log.Information("A aplicação começou.");
+            Log.Warning("Este é um aviso.");
+            Log.Error("Um erro ocorreu no processo.");
+
+            Console.WriteLine("Logs gerados. Verifique o arquivo serilog-log.txt.");
+
+            // Encerrando o logger
+            Log.CloseAndFlush();
+        }
+    }
+}
