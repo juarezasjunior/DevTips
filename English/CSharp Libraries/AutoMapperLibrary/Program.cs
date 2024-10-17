7fe12d263f7f1e47e5b34583712154506fdfe504
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-17 08:52:52 -0300

    AutoMapper

---
+++ English/CSharp Libraries/AutoMapperLibrary/Program.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using System;
+
+namespace AutoMapperExample
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configuring AutoMapper
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>());
+            var mapper = config.CreateMapper();
+
+            // Creating a Product object
+            Product product = new Product { Id = 1, Name = "Laptop", Price = 1500.99m };
+
+            // Mapping Product to ProductDTO
+            ProductDTO productDTO = mapper.Map<ProductDTO>(product);
+
+            Console.WriteLine($"Name: {productDTO.Name}, Price: {productDTO.Price}");
+        }
+    }
+}
---
+++ Portugues/CSharp Libraries/AutoMapperLibrary/Program.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using System;
+
+namespace AutoMapperExemplo
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+    public class ProdutoDTO
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configurando o AutoMapper
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Produto, ProdutoDTO>());
+            var mapper = config.CreateMapper();
+
+            // Criando um objeto Produto
+            Produto produto = new Produto { Id = 1, Nome = "Laptop", Preco = 1500.99m };
+
+            // Mapeando Produto para ProdutoDTO
+            ProdutoDTO produtoDTO = mapper.Map<ProdutoDTO>(produto);
+
+            Console.WriteLine($"Nome: {produtoDTO.Nome}, Preco: {produtoDTO.Preco}");
+        }
+    }
+}
