6a6278356eb2f00dea85e15d50f7c7d2c10815a5
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-18 10:07:03 -0300

    Mapster

---
+++ English/CSharp Libraries/MapsterLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿using Mapster;
+using System;
+
+namespace MapsterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the entity Product
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Laptop",
+                Price = 3500.99M
+            };
+
+            // Configuring Mapster
+            MapsterConfig.ConfigureMappings();
+
+            // Mapping to the ProductDTO using Mapster
+            var productDto = product.Adapt<ProductDTO>();
+
+            // Displaying the mapped data in the console
+            Console.WriteLine($"ProductDTO: Id={productDto.Id}, Name={productDto.Name}, PriceFormatted={productDto.PriceFormatted}");
+        }
+    }
+
+    // Entity class Product
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // DTO class ProductDTO
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PriceFormatted { get; set; }
+    }
+
+    // Configuring the mapping to format the price
+    public class MapsterConfig
+    {
+        public static void ConfigureMappings()
+        {
+            TypeAdapterConfig<Product, ProductDTO>.NewConfig()
+                .Map(dest => dest.PriceFormatted, src => src.Price.ToString("C2"));
+        }
+    }
+}
---
+++ Portugues/CSharp Libraries/MapsterLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿using Mapster;
+using System;
+
+namespace MapsterExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando uma instância da entidade Produto
+            var produto = new Produto
+            {
+                Id = 1,
+                Nome = "Notebook",
+                Preco = 3500.99M
+            };
+
+            // Configurando o Mapster
+            MapsterConfig.ConfigurarMapeamentos();
+
+            // Mapeando para o DTO ProdutoDTO usando Mapster
+            var produtoDto = produto.Adapt<ProdutoDTO>();
+
+            // Exibindo os dados mapeados no console
+            Console.WriteLine($"ProdutoDTO: Id={produtoDto.Id}, Nome={produtoDto.Nome}, Preço={produtoDto.PrecoFormatado}");
+        }
+    }
+
+    // Classe de entidade Produto
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+    // Classe de DTO ProdutoDTO
+    public class ProdutoDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string PrecoFormatado { get; set; }
+    }
+
+    // Configurando o mapeamento para formatar o preço
+    public class MapsterConfig
+    {
+        public static void ConfigurarMapeamentos()
+        {
+            TypeAdapterConfig<Produto, ProdutoDTO>.NewConfig()
+                .Map(dest => dest.PrecoFormatado, src => src.Preco.ToString("C2"));
+        }
+    }
+}
