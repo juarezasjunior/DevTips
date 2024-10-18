f2ebde83f841dbc20d008479a7095ec04d67427b
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-18 08:52:43 -0300

    Moq

---
+++ English/CSharp Libraries/MoqLibrary/Program.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+// Interface to be mocked
+public interface ICalculator
+{
+    int Add(int a, int b);
+}
+
+// Class that uses the interface
+public class Operations
+{
+    private readonly ICalculator _calculator;
+
+    public Operations(ICalculator calculator)
+    {
+        _calculator = calculator;
+    }
+
+    public int ExecuteSum(int a, int b)
+    {
+        return _calculator.Add(a, b);
+    }
+}
+
+public class OperationsTests
+{
+    [Fact]
+    public void ExecuteSum_ShouldReturnCorrectSum()
+    {
+        // Creating the mock for the ICalculator interface
+        var mockCalculator = new Mock<ICalculator>();
+
+        // Defining the expected behavior for the Add method
+        mockCalculator.Setup(c => c.Add(2, 3)).Returns(5);
+
+        // Using the mock in the Operations class
+        var operations = new Operations(mockCalculator.Object);
+        var result = operations.ExecuteSum(2, 3);
+
+        // Verifying if the result is as expected
+        Assert.Equal(5, result);
+
+        // Verifying if the Add method was called exactly once
+        mockCalculator.Verify(c => c.Add(2, 3), Times.Once);
+    }
+}
---
+++ Portugues/CSharp Libraries/MoqLibrary/Program.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+// Interface que será mockada
+public interface ICalculadora
+{
+    int Somar(int a, int b);
+}
+
+// Classe que utiliza a interface
+public class Operacoes
+{
+    private readonly ICalculadora _calculadora;
+
+    public Operacoes(ICalculadora calculadora)
+    {
+        _calculadora = calculadora;
+    }
+
+    public int ExecutarSoma(int a, int b)
+    {
+        return _calculadora.Somar(a, b);
+    }
+}
+
+public class OperacoesTests
+{
+    [Fact]
+    public void ExecutarSoma_DeveRetornarSomaCorreta()
+    {
+        // Criando o mock da interface ICalculadora
+        var mockCalculadora = new Mock<ICalculadora>();
+
+        // Definindo o comportamento esperado para o método Somar
+        mockCalculadora.Setup(c => c.Somar(2, 3)).Returns(5);
+
+        // Usando o mock na classe Operacoes
+        var operacoes = new Operacoes(mockCalculadora.Object);
+        var resultado = operacoes.ExecutarSoma(2, 3);
+
+        // Verificando se o resultado é o esperado
+        Assert.Equal(5, resultado);
+
+        // Verificando se o método Somar foi chamado exatamente uma vez
+        mockCalculadora.Verify(c => c.Somar(2, 3), Times.Once);
+    }
+}
