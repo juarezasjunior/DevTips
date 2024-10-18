f400a63912c13365650c5b1d4532737f13cd4640
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-18 08:30:05 -0300

    MassTransit

---
+++ English/CSharp Libraries/MassTransitLibrary/Program.cs
@@ -0,0 +1,62 @@
+﻿using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace MassTransitExample
+{
+    // Defining the message
+    public class OrderCreated
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    // Defining the message consumer
+    public class OrderCreatedConsumer : IConsumer<OrderCreated>
+    {
+        public Task Consume(ConsumeContext<OrderCreated> context)
+        {
+            Console.WriteLine($"Order received: {context.Message.Id} at {context.Message.Date}");
+            return Task.CompletedTask;
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Configuring MassTransit with RabbitMQ
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("rabbitmq://localhost", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                // Configuring the consumer
+                cfg.ReceiveEndpoint("order_queue", e =>
+                {
+                    e.Consumer<OrderCreatedConsumer>();
+                });
+            });
+
+            // Starting the bus
+            await busControl.StartAsync();
+
+            try
+            {
+                // Publishing a message
+                var order = new OrderCreated { Id = Guid.NewGuid().ToString(), Date = DateTime.Now };
+                await busControl.Publish(order);
+
+                Console.WriteLine("Order published.");
+                Console.ReadLine();
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
---
+++ Portugues/CSharp Libraries/MassTransitLibrary/Program.cs
@@ -0,0 +1,62 @@
+﻿using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace MassTransitExemplo
+{
+    // Definindo a mensagem
+    public class PedidoCriado
+    {
+        public string Id { get; set; }
+        public DateTime Data { get; set; }
+    }
+
+    // Definindo o consumidor da mensagem
+    public class PedidoCriadoConsumer : IConsumer<PedidoCriado>
+    {
+        public Task Consume(ConsumeContext<PedidoCriado> context)
+        {
+            Console.WriteLine($"Pedido recebido: {context.Message.Id} em {context.Message.Data}");
+            return Task.CompletedTask;
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Configurando o MassTransit com RabbitMQ
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("rabbitmq://localhost", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                // Configurando o consumidor
+                cfg.ReceiveEndpoint("pedido_queue", e =>
+                {
+                    e.Consumer<PedidoCriadoConsumer>();
+                });
+            });
+
+            // Iniciando o bus
+            await busControl.StartAsync();
+
+            try
+            {
+                // Publicando uma mensagem
+                var pedido = new PedidoCriado { Id = Guid.NewGuid().ToString(), Data = DateTime.Now };
+                await busControl.Publish(pedido);
+
+                Console.WriteLine("Pedido publicado.");
+                Console.ReadLine();
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
