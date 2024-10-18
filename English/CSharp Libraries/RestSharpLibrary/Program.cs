0cfd2e0b98389b2f1b7e52bcd231bfc47f1c71e1
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-18 09:23:32 -0300

    RestSharp

---
+++ English/CSharp Libraries/RestSharpLibrary/Program.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System;
+
+namespace RestSharpExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Creating the RestSharp client
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/posts/1", Method.Get);
+
+            // Executing the request
+            var response = await client.ExecuteAsync<Post>(request);
+
+            if (response.IsSuccessful)
+            {
+                var post = response.Data;
+                Console.WriteLine($"Title: {post.Title}\nContent: {post.Body}");
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+
+    // Class to map the response
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Portugues/CSharp Libraries/RestSharpLibrary/Program.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System;
+
+namespace RestSharpExemplo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Criando o cliente RestSharp
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/posts/1", Method.Get);
+
+            // Executando a requisição
+            var response = await client.ExecuteAsync<Post>(request);
+
+            if (response.IsSuccessful)
+            {
+                var post = response.Data;
+                Console.WriteLine($"Título: {post.Title}\nConteúdo: {post.Body}");
+            }
+            else
+            {
+                Console.WriteLine($"Erro: {response.StatusCode}");
+            }
+        }
+    }
+
+    // Classe para mapear a resposta
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
