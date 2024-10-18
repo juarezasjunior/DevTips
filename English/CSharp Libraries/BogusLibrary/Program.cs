84d0910e9c1cc0db6b465be0934da483840c1f31
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-18 09:55:47 -0300

    Bogus

---
+++ English/CSharp Libraries/BogusLibrary/Program.cs
@@ -0,0 +1,37 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+
+namespace BogusExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fake data generator for the Person class
+            var faker = new Faker<Person>()
+                .RuleFor(p => p.Name, f => f.Name.FullName())
+                .RuleFor(p => p.Email, f => f.Internet.Email())
+                .RuleFor(p => p.Age, f => f.Random.Int(18, 60))
+                .RuleFor(p => p.Address, f => f.Address.FullAddress());
+
+            // Generating a list of 5 people
+            var people = faker.Generate(5);
+
+            // Displaying the generated data in the console
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Name: {person.Name}, Email: {person.Email}, Age: {person.Age}, Address: {person.Address}");
+            }
+        }
+    }
+
+    // Class to represent a person
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Portugues/CSharp Libraries/BogusLibrary/Program.cs
@@ -0,0 +1,37 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+
+namespace BogusExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Gerador de dados falsos para a classe Pessoa
+            var faker = new Faker<Pessoa>()
+                .RuleFor(p => p.Nome, f => f.Name.FullName())
+                .RuleFor(p => p.Email, f => f.Internet.Email())
+                .RuleFor(p => p.Idade, f => f.Random.Int(18, 60))
+                .RuleFor(p => p.Endereco, f => f.Address.FullAddress());
+
+            // Gerando uma lista de 5 pessoas
+            var pessoas = faker.Generate(5);
+
+            // Exibindo os dados gerados no console
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine($"Nome: {pessoa.Nome}, Email: {pessoa.Email}, Idade: {pessoa.Idade}, Endereço: {pessoa.Endereco}");
+            }
+        }
+    }
+
+    // Classe para representar a pessoa
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+        public string Endereco { get; set; }
+    }
+}
