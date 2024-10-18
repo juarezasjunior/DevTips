e5bba0d1a22f57c995366794984e5365289feb36
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-18 09:43:33 -0300

    Flurl

---
+++ English/CSharp Libraries/FlurlLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿using Flurl.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace FlurlExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Making a GET request using Flurl
+            var post = await "https://jsonplaceholder.typicode.com/posts/1"
+                .GetJsonAsync<Post>();
+
+            // Displaying the data in the console
+            Console.WriteLine($"Title: {post.Title}\nContent: {post.Body}");
+        }
+    }
+
+    // Class to map the response
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Portugues/CSharp Libraries/FlurlLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿using Flurl.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace FlurlExemplo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Fazendo uma requisição GET usando Flurl
+            var post = await "https://jsonplaceholder.typicode.com/posts/1"
+                .GetJsonAsync<Post>();
+
+            // Exibindo os dados no console
+            Console.WriteLine($"Título: {post.Title}\nConteúdo: {post.Body}");
+        }
+    }
+
+    // Classe para mapear a resposta
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
