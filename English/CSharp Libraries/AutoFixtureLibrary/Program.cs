c1fa56874a6d213409c67d4e91b630e6c03be1e4
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-10-18 10:15:22 -0300

    AutoFixture

---
+++ English/CSharp Libraries/AutoFixtureLibrary/Program.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using System;
+
+namespace AutoFixtureExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the AutoFixture generator
+            var fixture = new Fixture();
+
+            // Generating a filled instance of the Order class
+            var order = fixture.Create<Order>();
+
+            // Displaying the automatically generated data
+            Console.WriteLine($"Order Id: {order.Id}, Total Value: {order.TotalValue}, Customer: {order.Customer.Name}");
+        }
+    }
+
+    // Order class
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal TotalValue { get; set; }
+        public Customer Customer { get; set; }
+    }
+
+    // Customer class
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Portugues/CSharp Libraries/AutoFixtureLibrary/Program.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using System;
+
+namespace AutoFixtureExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando uma instância do gerador AutoFixture
+            var fixture = new Fixture();
+
+            // Gerando uma instância preenchida da classe Pedido
+            var pedido = fixture.Create<Pedido>();
+
+            // Exibindo os dados gerados automaticamente
+            Console.WriteLine($"Pedido Id: {pedido.Id}, Valor: {pedido.ValorTotal}, Cliente: {pedido.Cliente.Nome}");
+        }
+    }
+
+    // Classe Pedido
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public decimal ValorTotal { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+
+    // Classe Cliente
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
