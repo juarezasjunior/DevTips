96ea53f64a11c2da7c86639624b3a91ebd9f5bf9
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-11-04 22:19:57 -0300

    SimplifiedNullableTypes

---
+++ English/CSharp/SimplifiedNullableTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+public class Person
+{
+    public string Name { get; set; }
+    public string? Nickname { get; set; }  // Can accept null
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person { Name = "John", Nickname = null };
+        Console.WriteLine($"Name: {person.Name}, Nickname: {person.Nickname ?? "None"}");
+    }
+}
---
+++ Portugues/CSharp/SimplifiedNullableTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public string? Apelido { get; set; }  // Pode aceitar null
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Pessoa pessoa = new Pessoa { Nome = "João", Apelido = null };
+        Console.WriteLine($"Nome: {pessoa.Nome}, Apelido: {pessoa.Apelido ?? "Nenhum"}");
+    }
+}
