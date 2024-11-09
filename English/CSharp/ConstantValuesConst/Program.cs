d6fff012e8aba0026ccfcdd025f9327dd6605360
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-11-09 00:05:12 -0300

    ConstantValuesConst

---
+++ English/CSharp/ConstantValuesConst/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Math
+{
+    public const double PI = 3.14159;
+
+    public double CalculateCircumference(double radius)
+    {
+        return 2 * PI * radius;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Math math = new Math();
+        double circumference = math.CalculateCircumference(5);
+        Console.WriteLine($"Circumference: {circumference}");
+    }
+}
---
+++ Portugues/CSharp/ConstantValuesConst/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Matematica
+{
+    public const double PI = 3.14159;
+
+    public double CalcularCircunferencia(double raio)
+    {
+        return 2 * PI * raio;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Matematica matematica = new Matematica();
+        double circunferencia = matematica.CalcularCircunferencia(5);
+        Console.WriteLine($"Circunferência: {circunferencia}");
+    }
+}
