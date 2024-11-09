6628a38c39fd6bfe7c4bf9e486df4196182f3b1d
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-11-08 23:46:20 -0300

    ImmutableConstantsReadonly

---
+++ English/CSharp/ImmutableConstantsReadonly/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Circle
+{
+    public readonly double PI;
+
+    public Circle()
+    {
+        PI = 3.14159;
+    }
+
+    public double CalculateArea(double radius)
+    {
+        return PI * radius * radius;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Circle circle = new Circle();
+        double area = circle.CalculateArea(5);
+        Console.WriteLine($"Circle area: {area}");
+    }
+}
---
+++ Portugues/CSharp/ImmutableConstantsReadonly/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Circulo
+{
+    public readonly double PI;
+
+    public Circulo()
+    {
+        PI = 3.14159;
+    }
+
+    public double CalcularArea(double raio)
+    {
+        return PI * raio * raio;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Circulo circulo = new Circulo();
+        double area = circulo.CalcularArea(5);
+        Console.WriteLine($"Área do círculo: {area}");
+    }
+}
