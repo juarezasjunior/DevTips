a6064d6ea098b82326a805a9494c39a4a4797800
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-11-10 22:07:39 -0300

    StringBuilderConcatenation

---
+++ English/CSharp/StringBuilderConcatenation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        StringBuilder text = new StringBuilder();
+
+        text.AppendLine("First line of the text.");
+        text.AppendLine("Second line of the text.");
+        text.AppendLine("Third line of the text.");
+
+        Console.WriteLine(text.ToString());
+    }
+}
---
+++ Portugues/CSharp/StringBuilderConcatenation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        StringBuilder texto = new StringBuilder();
+
+        texto.AppendLine("Primeira linha do texto.");
+        texto.AppendLine("Segunda linha do texto.");
+        texto.AppendLine("Terceira linha do texto.");
+
+        Console.WriteLine(texto.ToString());
+    }
+}
