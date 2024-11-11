121652c4deeffc52122e67f1ab78168b83ad7dd6
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-11-10 22:15:33 -0300

    ResourceReleaseUsing

---
+++ English/CSharp/ResourceReleaseUsing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (StreamWriter writer = new StreamWriter("file.txt"))
+        {
+            writer.WriteLine("Writing to the file using the using block.");
+        }
+        // The StreamWriter is automatically released after the block ends
+    }
+}
---
+++ Portugues/CSharp/ResourceReleaseUsing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (StreamWriter escritor = new StreamWriter("arquivo.txt"))
+        {
+            escritor.WriteLine("Escrevendo no arquivo usando o bloco using.");
+        }
+        // O StreamWriter é automaticamente liberado após o fim do bloco
+    }
+}
