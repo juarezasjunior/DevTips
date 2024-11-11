28917d7ba7d04fc1e81a5f9f0cb2511cfa3ea7ef
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-11-10 22:00:39 -0300

    StringInterpolation

---
+++ English/CSharp/StringInterpolation/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string name = "John";
+        int age = 30;
+
+        // Using interpolation instead of concatenation
+        string message = $"Name: {name}, Age: {age}";
+
+        Console.WriteLine(message);
+    }
+}
---
+++ Portugues/CSharp/StringInterpolation/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string nome = "João";
+        int idade = 30;
+
+        // Usando interpolação ao invés de concatenação
+        string mensagem = $"Nome: {nome}, Idade: {idade}";
+
+        Console.WriteLine(mensagem);
+    }
+}
