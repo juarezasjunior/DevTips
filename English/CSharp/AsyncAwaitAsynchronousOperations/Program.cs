5c3037a997a51480f8ee35143305699c4031296d
Juarez Júnior <juarez.a.s.junior@gmail.com>
2024-11-15 07:30:09 -0300

    AsyncAwaitAsynchronousOperations

---
+++ English/CSharp/AsyncAwaitAsynchronousOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        string url = "https://api.github.com/repos/dotnet/runtime";
+
+        using HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "C# App");
+
+        string response = await client.GetStringAsync(url);
+
+        Console.WriteLine("API Data:");
+        Console.WriteLine(response);
+    }
+}
